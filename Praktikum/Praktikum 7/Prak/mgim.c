2880dff4bbcf8d6582a4e20f7e5be610f75c0616
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2023-12-06 15:08:24 +0700

    benerin prak 7 8

--- Praktikum/Praktikum 7/Prak/mgim.c
+++ Praktikum/Praktikum 7/Prak/mgim.c
@@ -1,36 +1,89 @@
-#include "queue.h"
 #include <stdio.h>
+#include "queue.h"
 
-int main(){
-    int N, Ni;
-    int result = 1; // Initialize result with 1
-
-    // Input the number of elements
-    scanf("%d", &N);
-
-    // Process each Ni
-    for (int i = 0; i < N; i++){
-        Queue q;
-        CreateQueue(&q);
-
-        scanf("%d", &Ni);
-
-        enqueue(&q, Ni);
-
-        int Ri = 1;
-        while (!isEmpty(q)){
-            int val;
-            dequeue(&q, &val);
-            if (val > 0){
-                enqueue(&q, val - 1);
-            }
-            Ri++;
-        }
-
-        result *= Ri;
+int max(int a[]) {
+    if (a[0] > a[1]) {
+        return a[0];
+    } else {
+        return a[1];
     }
+}
 
-    printf("%d\n", result);
+void fway(int n , int r,Queue *Q,int *prev){
+    int a;
+    if(n == 0){
+        if(r < *prev){
+            if(!isEmpty(*Q)){
+                dequeue(Q,&a);
+            }
+            enqueue(Q,r);
+            *prev = r;
+        }
+        
+    }else{
 
+        r++;
+        
+        fway(n-1,r,Q,prev);
+        int temp1[2];
+       
+        int j;
+        int result = n;
+        boolean step2 = false;
+        for (j = 2; j < n; j++) {
+            if (n % j == 0) {
+                temp1[0] = j;
+                temp1[1] = n / j;
+                if (max(temp1) < result) {
+                    result = max(temp1);
+                    step2 = true;
+                }
+            }
+        }
+        if(step2){
+            fway(result,r,Q,prev);
+        }
+    }
+}
+
+int minimumQueue(Queue *q){
+    int result;
+    dequeue(q,&result);
+    int temp;
+    while(!isEmpty(*q)){
+        dequeue(q,&temp);
+        if(temp < result){
+            result = temp;
+        }
+    }
+    return result;
+}
+
+int main() { 
+    int n;
+    scanf("%d", &n);
+    int i;
+    int temp;
+    
+    int hasil = 1;
+    Queue q;
+    int tempR;
+    
+    for (i = 0; i < n; i++) {
+        int r =0;
+        CreateQueue(&q);
+        scanf("%d",&temp);
+        int prev = temp;
+        fway(temp,r,&q,&prev);
+        int a;
+        
+        hasil = hasil * prev;
+    }
+    if(n == 0){
+        printf("0\n");
+    }else{
+        printf("%d\n", hasil);
+    }
+    
     return 0;
 }
